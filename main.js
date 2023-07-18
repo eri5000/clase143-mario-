b0cdc83990715102fc791b104387459cdf0bc8a1
eri5000 <107961194+eri5000@users.noreply.github.com>
2023-07-18 16:39:10 -0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+
+noseX="";
+noseY="";
+gameStatus="";
+
+function preload() {
+	world_start = loadSound("world_start.wav");
+	mario_jump = loadSound("jump.wav");
+	mario_coin = loadSound("coin.wav");
+	mario_gameover = loadSound("gameover.wav");
+	mario_kick = loadSound("kick.wav");
+	mario_die = loadSound("mariodie.wav");
+	setSprites();
+	MarioAnimation();
+}
+
+function setup() {
+	canvas = createCanvas(1240,336);
+	instializeInSetup(mario);
+	canvas.parent('canvas');
+	video=createCapture(VIDEO);
+	video.size(650,300);
+	video.parent('game_console');
+	poseNet=ml5.poseNet(video,modelLoaded);
+	poseNet.on('pose',gotPoses);
+}
+
+function draw() {
+	game()
+}
+function modelLoaded(){
+	console.log("modelo cargado");
+
+}
+function gotPoses(results){
+ if(results.length>0){
+	console.log(results);
+	noseX=results[0].pose.nose.x;
+	noseY=results[0].pose.nose.y;
+ }
+}
+function startGame(){
+  gameStatus="start";
+  document.getElementById("status").innerHTML="El juego esta cargando";
+  
+}
+function game(){
+	console.log("noseX = " + noseX + "noseY = " + noseY);
+}
+
+
+
+
+
+
+
